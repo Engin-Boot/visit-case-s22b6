fb71e7569640b7b7db8b2e24e08ef7803eb0e4d7
Chethan S V <Chethan.SV@philips.com>
2020-09-14 09:44:55 +0530

    Added ReceiverTest

---
+++ Receiver/Receiver.Test/ReceiverTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Receiver.Receiver.Test
+{
+    public class ReceiverTest
+    {
+        footfall foot = new footfall();
+        [Fact]
+        public void Testgetaverageperday()
+        {
+           int avgDay= (int)foot.getaverageperday(1097);
+           Assert.Equal(45, avgDay);
+        }
+
+        [Fact]
+        public void Testgetaverageperweak()
+        {
+            int avgWeek = (int)foot.getaverageperweak(1);
+            Assert.Equal(23, avgWeek);
+        }
+
+        [Fact]
+        public void Testgetpeekinmonth()
+        {
+            int peakMonth = foot.getpeekinmonth(1);
+            Assert.Equal(1097, peakMonth);
+        }
+    }
+}

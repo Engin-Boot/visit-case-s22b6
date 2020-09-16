c3d561edd47816aa0bf13f3c4ade9959570e071d
Chethan S V <Chethan.SV@philips.com>
2020-09-16 06:17:13 +0530

    Updated Sender

--- Sender/Program.cs
+++ Sender/Program.cs
@@ -11,14 +11,13 @@ namespace Sender
             {
                 string filePath = @"c:\csvfolder\casestudy.csv";
                 StreamReader source = new StreamReader(filePath);
-                var lines = new List<string[]>();
                 List<string> infoFootfall = new List<string>();
-                string Line;
-                while ((Line = source.ReadLine()) != null)
+                string line;
+                while ((line = source.ReadLine()) != null)
                 {
                     //string[] line = source.ReadLine().Split(',');
                     // lines.Add(line);
-                    infoFootfall.Add(Line);
+                    infoFootfall.Add(line);
                 }
                 PrintOnConsole printOnConsole = new PrintOnConsole();
                 printOnConsole.PrintingInfoOnConsole(infoFootfall);
